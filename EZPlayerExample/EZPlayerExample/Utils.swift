cc02ce2ca185f0f507261f2c6108f20ae9466072
easyui <522941939@qq.com>
2017-03-15 17:43:19 +0800

    static let remoteMP4_0 =  URL(string: "http://baobab.wdjcdn.com/1456459181808howtoloseweight_x264.mp4")!

--- EZPlayerExample/EZPlayerExample/Utils.swift
+++ EZPlayerExample/EZPlayerExample/Utils.swift
@@ -18,7 +18,7 @@ extension URL {
         static let apple_0 = URL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")!
         static let apple_1 = URL(string: "https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8")!
 
-        static let remoteMP4_0 =  URL(string: "http://gslb.miaopai.com/stream/kPzSuadRd2ipEo82jk9~sA__.mp4")!
+        static let remoteMP4_0 =  URL(string: "http://baobab.wdjcdn.com/1456459181808howtoloseweight_x264.mp4")!
 
         static let remoteM3U8_0 =  URL(string: "http://www.streambox.fr/playlists/x36xhzz/url_6/193039199_mp4_h264_aac_hq_7.m3u8")!
 
